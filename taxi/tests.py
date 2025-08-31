1d51c7cf3d19114bc69fd4ba9a9c2987181fcf6f
benfict <benfict@gmail.com>
2025-09-01 01:49:30 +0300

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,72 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from taxi.models import Car, Manufacturer, Driver
 
-# Create your tests here.
+User = get_user_model()
+
+
+class SearchTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser",
+            password="12345",
+            license_number="123456"
+        )
+        self.client = Client()
+        self.client.login(username="testuser", password="12345")
+
+        self.manufacturer1 = Manufacturer.objects.create(
+            name="BMW",
+            country="Germany"
+        )
+        self.manufacturer2 = Manufacturer.objects.create(
+            name="Audi",
+            country="Germany"
+        )
+
+        self.car1 = Car.objects.create(
+            model="X5",
+            manufacturer=self.manufacturer1
+        )
+        self.car2 = Car.objects.create(
+            model="Q7",
+            manufacturer=self.manufacturer2
+        )
+
+        self.driver1 = User.objects.create_user(
+            username="john",
+            license_number="654321"
+        )
+        self.driver2 = User.objects.create_user(
+            username="mary",
+            license_number="987654"
+        )
+
+    def test_car_search(self):
+        url = reverse("taxi:car-list")
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        cars = response.context["car_list"]
+        self.assertIn(self.car1, cars)
+        self.assertIn(self.car2, cars)
+
+        response = self.client.get(url, {"model": "X5"})
+        cars = response.context["car_list"]
+        self.assertIn(self.car1, cars)
+        self.assertNotIn(self.car2, cars)
+
+    def test_driver_search(self):
+        url = reverse("taxi:driver-list")
+        response = self.client.get(url, {"username": "john"})
+        drivers = response.context["driver_list"]
+        self.assertIn(self.driver1, drivers)
+        self.assertNotIn(self.driver2, drivers)
+
+    def test_manufacturer_search(self):
+        url = reverse("taxi:manufacturer-list")
+        response = self.client.get(url, {"name": "BMW"})
+        manufacturers = response.context["manufacturer_list"]
+        self.assertIn(self.manufacturer1, manufacturers)
+        self.assertNotIn(self.manufacturer2, manufacturers)
--- taxi/views.py
+++ taxi/views.py
@@ -6,7 +6,14 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
+from .forms import (
+    DriverCreationForm,
+    DriverLicenseUpdateForm,
+    CarForm,
+    CarSearchForm,
+    ManufacturerSearchForm,
+    DriverSearchForm
+)
 
 
 @login_required
@@ -36,6 +43,18 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(ManufacturerListView, self).get_context_data(**kwargs)
+        context["search_form"] = ManufacturerSearchForm(self.request.GET)
+        return context
+
+    def get_queryset(self):
+        form = ManufacturerSearchForm(self.request.GET)
+        queryset = Manufacturer.objects.all()
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+        return queryset
+
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
@@ -58,6 +77,18 @@ class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
     queryset = Car.objects.select_related("manufacturer")
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CarListView, self).get_context_data(**kwargs)
+        context["search_form"] = CarSearchForm()
+        return context
+
+    def get_queryset(self):
+        queryset = Car.objects.select_related("manufacturer")
+        form = CarSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(model__icontains=form.cleaned_data["model"])
+        return queryset
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
@@ -84,6 +115,20 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 class DriverListView(LoginRequiredMixin, generic.ListView):
     model = Driver
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(DriverListView, self).get_context_data(**kwargs)
+        context["search_form"] = DriverSearchForm(self.request.GET)
+        return context
+
+    def get_queryset(self):
+        form = DriverSearchForm(self.request.GET)
+        queryset = Driver.objects.all()
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
